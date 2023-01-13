5f7b9f8fa7229aff1f36ad700a9603b629f0b920
Evan Mattiza <emattiza@users.noreply.github.com>
2023-01-13 08:57:59 -0600

    Fun/add cellular (#1)
    * add celullar
    
    * add game of life

---
+++ modules/visuals/cellular-automaton.nix
@@ -0,0 +1,41 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+with lib;
+with builtins; let
+  cfg = config.vim.visuals;
+in {
+  options.vim.visuals = {
+    cellular-automaton = {
+      enable = mkOption {
+        type = types.bool;
+        description = "some fun for your neovim";
+      };
+    };
+  };
+
+  config =
+    mkIf cfg.enable
+    {
+      vim.startPlugins = with pkgs.neovimPlugins; [
+        (
+          if cfg.cellular-automaton.enable
+          then cellular-automaton
+          else null
+        )
+      ];
+      vim.luaConfigRC = ''
+        ${
+          if cfg.cellular-automaton.enable
+          then ''
+            vim.keymap.set("n", "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>")
+            vim.keymap.set("n", "<leader>fmg", "<cmd>CellularAutomaton game_of_life<CR>")
+          ''
+          else ""
+        }
+      '';
+    };
+}
