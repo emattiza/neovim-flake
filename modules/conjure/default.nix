9828a1981952b740cebc072c80bbe82a80ef0cfe
emattiza <emattiza@gmail.com>
2023-04-11 16:39:52 -0500

    finished conjure addition

--- modules/conjure/default.nix
+++ modules/conjure/default.nix
@@ -3,4 +3,26 @@
   lib,
   config,
   ...
-}: {}
+}: let
+  cfg = config.vim.conjure;
+  writeIf = cond: msg:
+    if cond
+    then msg
+    else "";
+in
+  with lib; {
+    options = {
+      vim = {
+        conjure = {
+          enable = mkEnableOption "enable Conjure";
+        };
+      };
+    };
+    config = mkIf (cfg.enable) {
+      vim.startPlugins = with pkgs.neovimPlugins; [
+        conjure
+      ];
+      vim.luaConfigRC = ''
+      '';
+    };
+  }
