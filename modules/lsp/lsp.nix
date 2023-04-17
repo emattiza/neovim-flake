ee61fc817679b2a088fec94b64cf85c4936bebf5
emattiza <emattiza@gmail.com>
2023-04-16 23:43:27 -0500

    cant put lua in vimscript places...

--- modules/lsp/lsp.nix
+++ modules/lsp/lsp.nix
@@ -94,39 +94,6 @@ in {
 
         configRC = ''
           ${
-            if cfg.rust.enable
-            then ''
-              local rt = require('rust-tools')
-              rust_on_attach = function(client, bufnr)
-                default_on_attach(client, bufnr)
-                local opts = { noremap=true, silent=true, buffer = bufnr }
-                vim.keymap.set("n", "<leader>ris", rt.inlay_hints.set, opts)
-                vim.keymap.set("n", "<leader>riu", rt.inlay_hints.unset, opts)
-                vim.keymap.set("n", "<leader>rr", rt.runnables.runnables, opts)
-                vim.keymap.set("n", "<leader>rp", rt.parent_module.parent_module, opts)
-                vim.keymap.set("n", "<leader>rm", rt.expand_macro.expand_macro, opts)
-                vim.keymap.set("n", "<leader>rc", rt.open_cargo_toml.open_cargo_toml, opts)
-                vim.keymap.set("n", "<leader>rg", function() rt.crate_graph.view_crate_graph("x11", nil) end, opts)
-              end
-              local rustopts = {
-                tools = {
-                  autoSetHints = true,
-                  hover_with_actions = false,
-                  inlay_hints = {
-                    only_current_line = false,
-                  }
-                },
-                server = {
-                  capabilities = capabilities,
-                  on_attach = rust_on_attach,
-                }
-              }
-              rt.setup(rustopts)
-            ''
-            else ""
-          }
-
-          ${
             if cfg.nix
             then ''
               autocmd filetype nix setlocal tabstop=2 shiftwidth=2 softtabstop=2
@@ -360,6 +327,33 @@ in {
                 name = "crates.nvim",
               }
             }
+
+              local rt = require('rust-tools')
+              rust_on_attach = function(client, bufnr)
+                default_on_attach(client, bufnr)
+                local opts = { noremap=true, silent=true, buffer = bufnr }
+                vim.keymap.set("n", "<leader>ris", rt.inlay_hints.set, opts)
+                vim.keymap.set("n", "<leader>riu", rt.inlay_hints.unset, opts)
+                vim.keymap.set("n", "<leader>rr", rt.runnables.runnables, opts)
+                vim.keymap.set("n", "<leader>rp", rt.parent_module.parent_module, opts)
+                vim.keymap.set("n", "<leader>rm", rt.expand_macro.expand_macro, opts)
+                vim.keymap.set("n", "<leader>rc", rt.open_cargo_toml.open_cargo_toml, opts)
+                vim.keymap.set("n", "<leader>rg", function() rt.crate_graph.view_crate_graph("x11", nil) end, opts)
+              end
+              local rustopts = {
+                tools = {
+                  autoSetHints = true,
+                  hover_with_actions = false,
+                  inlay_hints = {
+                    only_current_line = false,
+                  }
+                },
+                server = {
+                  capabilities = capabilities,
+                  on_attach = rust_on_attach,
+                }
+              }
+              rt.setup(rustopts)
           ''}
 
           ${writeIf cfg.terraform ''
